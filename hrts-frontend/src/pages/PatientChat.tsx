c8358c73e390733b7d7bf1d711864eeb0cb8bbc1
Betselot Kidane <betselotkidane111@gmail.com>
2023-09-03 16:19:51 +0300

    UI implementation

--- hrts-frontend/src/pages/PatientChat.tsx
+++ hrts-frontend/src/pages/PatientChat.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import "./../../styles/patientChat.css"
 
 const PatientChat = () => {
   return (
-    <div>PatientChat</div>
-  )
-}
+    <div className="patient-chat">
+      <div className="chat-messages">
+        <div className="message patient">
+          <div className="message-content">I am not filling well</div>
+          <div className="message-timestamp">10:00 AM</div>
+        </div>
+        <div className="message doctor">
+        <div className="message-content">I'm sorry to hear that you're not feeling well. I'm here to help in any way I can. It's important to remember that I'm an AI language model and not a substitute for professional medical advice. If your condition is serious or concerning, I strongly recommend reaching out to a healthcare professional or seeking medical attention immediately.</div>
+          <div className="message-content">If you're comfortable sharing more details about how you're feeling, I can try to provide some general information or suggestions that may be helpful.</div>
+          <div className="message-timestamp">10:01 AM</div>
+        </div>
+        <div className="message patient">
+          <div className="message-content">I have a bad fever, and I can't even eat food.</div>
+          <div className="message-timestamp">10:02 AM</div>
+        </div>
+        <div className="message doctor">
+          <div className="message-content">Based on the symptoms you've described, it's possible that you may be experiencing a gastrointestinal infection or a viral illness. Gastroenteritis, commonly known as the stomach flu, is a common cause of symptoms such as fever, vomiting, and diarrhea. However, it's important to note that many different illnesses can cause these symptoms, so it's difficult to provide a definitive diagnosis without a proper medical evaluation.
 
-export default PatientChat
+In any case, if your symptoms are severe, persistent, or worsening, it's crucial to seek medical attention promptly. A healthcare professional will be able to evaluate your symptoms, perform any necessary tests, and provide an accurate diagnosis. They can also recommend the most appropriate treatment for your specific condition.
+
+Remember to stay hydrated by drinking plenty of fluids, even if you have difficulty eating. Oral rehydration solutions can be particularly helpful in replacing lost fluids and electrolytes. However, it's best to consult with a healthcare professional to determine the most suitable course of action for your situation.
+
+I hope you feel better soon!</div>
+          <div className="message-timestamp">10:03 AM</div>
+        </div>
+      </div>
+      <div className="chat-input">
+        <input type="text" placeholder="Type your message..." />
+        <button>Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default PatientChat;
--- hrts-frontend/src/pages/PatientMedication.tsx
+++ hrts-frontend/src/pages/PatientMedication.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+
+import MedicationCard from '../components/MedicationCard';
+import './../../styles/patientMedication.css';
 
 const PatientMedication = () => {
-  return (
-    <div>PatientMedication</div>
-  )
-}
+  const medications = [
+    {
+      type: 'Antibiotic',
+      description:
+        'Amoxicillin is an antibiotic used to treat various bacterial infections such as respiratory tract infections, ear infections, and urinary tract infections.',
+      startdate: Date.now(),
+      enddate: Date.now(),
+    },
+    {
+      type: 'Analgesic',
+      description:
+        'Ibuprofen is an analgesic medication commonly used to relieve pain, reduce inflammation, and lower fever.',
+      startdate: Date.now(),
+      enddate: Date.now(),
+    },
+    {
+      type: 'Antidepressant',
+      description:
+        'Sertraline is an antidepressant medication prescribed to treat depression, obsessive-compulsive disorder (OCD), panic disorder, and other mental health conditions.',
+      startdate: Date.now(),
+      enddate: Date.now(),
+    },
+    {
+      type: 'Antihistamine',
+      description:
+        'Loratadine is an antihistamine medication used to relieve allergy symptoms such as sneezing, runny nose, itching, and watery eyes.',
+      startdate: Date.now(),
+      enddate: Date.now(),
+    },
+    {
+      type: 'Anti-inflammatory',
+      description:
+        'Prednisone is an anti-inflammatory medication often prescribed to reduce inflammation and suppress the immune system in conditions such as asthma, arthritis, and autoimmune disorders.',
+      startdate: Date.now(),
+      enddate: Date.now(),
+    },
+  ];
 
-export default PatientMedication
+  return (
+    <div className="medication-container">
+      <h1>Patient Medication</h1>
+      <div className="medication-cards">
+        {medications.map((medication) => (
+          <MedicationCard medication={medication} key={medication.type} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PatientMedication;
